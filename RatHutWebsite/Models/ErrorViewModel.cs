14ed3450d3ea2c1b7a9569f3b8c305043de31d93
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-05 19:05:02 -0700

    Add project files.

---
+++ RatHutWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RatHutWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
