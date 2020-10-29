5580da6741a33f9976410eb72e429646ea7aac83
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2020-10-29 15:17:38 -0700

    Customers can now view the menu (#28)
    * Created a MenuController (for customers to see product listings)
    
    * Removed Create, Edit, Details, and Delete links from the Menu index view
    
    * Removed Create, Edit, Delete actions from MenuController
    
    * Deleted the view files for Create, Edit, Delete in Menu folder

---
+++ RatHutWebsite/Controllers/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RatHutWebsite.Data;
+using RatHutWebsite.Models;
+
+namespace RatHutWebsite.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Menu
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+
+        // GET: Menu/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
