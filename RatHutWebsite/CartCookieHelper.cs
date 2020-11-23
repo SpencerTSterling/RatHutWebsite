edb34b1db28c48fb288ab58e5e092edbe7dafc01
SethSterling <63669378+SethSterling@users.noreply.github.com>
2020-11-22 23:16:08 -0800

    Issue#10 ShoppingCart (#37)
    * Created CartController
    
    * Added links to CartController
    
    * Added CartCookieHelper
    
    * Made it so shopping cart link appears in menu page instead of product
    
    * Created Summary View
    
    * Implement cart cookie

---
+++ RatHutWebsite/CartCookieHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using RatHutWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RatHutWebsite
+{
+    public static class CartCookieHelper
+    {
+        const string CartCookie = "ShoppingCartCookie";
+        public static List<Product> GetCartProducts(IHttpContextAccessor http)
+        {
+            // Get existing cart items
+            string existingItems =
+                http.HttpContext.Request.Cookies[CartCookie];
+
+            List<Product> cartProducts = new List<Product>();
+            if (existingItems != null)
+            {
+                cartProducts =
+                    JsonConvert.DeserializeObject<List<Product>>(existingItems);
+            }
+            return cartProducts;
+        }
+
+        public static void AddProductToCart(IHttpContextAccessor http, Product p)
+        {
+            List<Product> cartProducts = GetCartProducts(http);
+            cartProducts.Add(p);
+            // Add product to cart cookie
+            string data = JsonConvert.SerializeObject(cartProducts);
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddYears(1),
+                Secure = true,
+                IsEssential = true
+            };
+
+            http.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+        }
+    }
+}
---
+++ RatHutWebsite/Controllers/CartController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RatHutWebsite.Data;
+using RatHutWebsite.Models;
+
+namespace RatHutWebsite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContext;
+        public CartController(ApplicationDbContext context, IHttpContextAccessor httpContext)
+        {
+            _context = context;
+            _httpContext = httpContext;
+        }
+
+        public async Task<IActionResult> Add(int id, string prevUrl)
+        {
+            // Get product from the database
+            Product p = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+
+            List<Product> cartProducts = CartCookieHelper.GetCartProducts(_httpContext);
+            CartCookieHelper.AddProductToCart(_httpContext, p);
+
+            TempData["Message"] = $"{p.Title} was added to your cart";
+
+            // Redirect back to previous page
+            return Redirect(prevUrl);
+        }
+
+        public IActionResult Summary()
+        {
+            List<Product> cartProducts =
+                CartCookieHelper.GetCartProducts(_httpContext);
+
+            return View(cartProducts);
+        }
+    }
+}
