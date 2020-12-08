4e06b346d9f64195acd8d6969d1581485da8e27f
Spencer Sterling <63821532+SpencerTSterling@users.noreply.github.com>
2020-12-07 18:32:08 -0800

    Issue#45 refactor product views (#46)
    * Refactored Product Index view to include Price and ImageURL
    
    * Added Price and ImageURL to all the CRUD pages for Product
    
    * Added Price for the Menu pages
    
    * Added price to the Cart pages

--- RatHutWebsite/Controllers/ProductsController.cs
+++ RatHutWebsite/Controllers/ProductsController.cs
@@ -56,7 +56,7 @@ namespace RatHutWebsite.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductId,Title,Description,Category,IsVegan,IsVegetarian")] Product product)
+        public async Task<IActionResult> Create([Bind("ProductId,Title,Description,Category,IsVegan,IsVegetarian,Price,ImageURL")] Product product)
         {
             if (ModelState.IsValid)
             {
@@ -88,7 +88,7 @@ namespace RatHutWebsite.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Title,Description,Category,IsVegan,IsVegetarian")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Title,Description,Category,IsVegan,IsVegetarian,Price,ImageURL")] Product product)
         {
             if (id != product.ProductId)
             {
